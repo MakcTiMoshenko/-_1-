264febda76c5fca18901f7188bd55169d886c8b3
Макс <KpuBopy4Ka86@gmail.com>
2023-02-12 11:27:04 +0300

    Вывод всех четных чисел из N

---
+++ Exemle4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все четные числа от 1 до N
+// 5 ->2,4
+// 8 -> 2,4.6,8
+
+System.Console.Write("Введите число N ");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i <= number; i=i+2)
+{
+    System.Console.Write(i + " ");
+}
