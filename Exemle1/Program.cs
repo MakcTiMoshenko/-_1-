b929cd79ccf5eec29437ec0f8e2c28c8007042a4
Макс <KpuBopy4Ka86@gmail.com>
2023-02-12 11:30:34 +0300

    Нахождение МАХ из 2-х чисел

---
+++ Exemle1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2 Напишите программу, которая на вход принимает 2 числа и выдает, Какое число больше, а какое меньше
+// a=5, b=7 -> max=7
+// a=2, b=10 -> max=10
+// a=-9, b=-3 -> max=-3 
+
+System.Console.Write("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 < number2)
+{
+    System.Console.Write("Max "+ number2);
+}
+else 
+{
+    System.Console.Write("Max "+ number1);
+}
