98736ad8ab5b3f0178cfb0c20d95935d05fe37fd
Макс <KpuBopy4Ka86@gmail.com>
2023-02-12 10:32:03 +0300

    Нахождение МАХ из 3-х чисел

---
+++ Exemple2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 4 Напишите программу, которая принимает на вход 3 числа и выдает максимальное из этих чисел
+// 2,3,7 -> 7
+// 44,5,78 -> 78
+// 22,3,9 -> 22
+
+System.Console.Write("Введите первое число1 ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число2 ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число3 ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (max < number2)
+{
+    max = number2;
+}
+else 
+{
+    max = number1;
+}
+if (max < number3)
+{
+    max = number3;
+}
+else 
+{
+    max = number1;
+}
+System.Console.Write("Max " + max);
